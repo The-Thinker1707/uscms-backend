89cc81d5921dc724d5f7ed84ea9aa55a58ca1698
The-Thinker1707 <saytontreyz@gmail.com>
2026-08-28 03:35:53 +0200

    Add files via upload

--- uscms-backend/src/main/java/com/uscms/controller/SportController.java
+++ uscms-backend/src/main/java/com/uscms/controller/SportController.java
@@ -1,6 +1,6 @@
-﻿package com.uscms.controller;
+package com.uscms.controller;
 
-import com.uscms.model.Sport;
+import com.uscms.entity.Sport;
 import com.uscms.repository.SportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,25 +15,21 @@ public class SportController {
     @Autowired
     private SportRepository sportRepository;
 
-    // GET all
     @GetMapping
     public List<Sport> getAllSports() {
         return sportRepository.findAll();
     }
 
-    // GET by ID
     @GetMapping("/{id}")
     public Sport getSportById(@PathVariable Long id) {
         return sportRepository.findById(id).orElse(null);
     }
 
-    // POST create
     @PostMapping
     public Sport createSport(@RequestBody Sport sport) {
         return sportRepository.save(sport);
     }
 
-    // DELETE by ID
     @DeleteMapping("/{id}")
     public void deleteSport(@PathVariable Long id) {
         sportRepository.deleteById(id);
